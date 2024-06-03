ce5e7bcd53e4152c0d3dacf9eb4d703b65a95be2
PippyHub <piper.innshall@gmail.com>
2024-06-03 22:27:40 +1200

    finished rules

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -5,8 +5,8 @@ val cols = 60
 val directions = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
 fun neighbor(y: Int, x: Int, grid: Grid): Int = directions.count { (dy, dx) -> grid.get((y + dy + rows) % rows).get((x + dx + cols) % cols) }
 fun set(y: Int, x: Int, grid: Grid): Boolean = if (grid[y][x]) neighbor(y, x, grid) in 2..3 else neighbor(y, x, grid) == 3
-fun step(grid: Grid): Grid = List(rows) { y -> List(cols) { x -> set(y, x, grid) } }
+fun evolve(grid: Grid): Grid = List(rows) { y -> List(cols) { x -> set(y, x, grid) } }
 fun render(grid: Grid): String = grid.joinToString("\n") { row -> row.joinToString(" ") { if (it) "#" else "-" } }
-fun game(grid: Grid): Unit = println("\u001b[H\u001b[2J" + render(grid)).also { Thread.sleep(100) }.let { game(step(grid)) }
 fun grid(file: File): Grid = List(rows) { y -> List(cols) { x -> file.readLines().map { it.split(",").map(String::toInt) }.any { it[0] == y && it[1] == x } } }
+fun game(grid: Grid): Unit = println("\u001b[H\u001b[2J" + render(grid)).also { Thread.sleep(100) }.apply { if (grid.flatten().any { it }) game(evolve(grid)) }
 fun main() = game(grid(File("src/main/resources/grid")))

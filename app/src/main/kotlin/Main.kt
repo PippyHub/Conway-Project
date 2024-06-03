0a399513ea45dce0c10412971f3b6a57adc46b33
PippyHub <piper.innshall@gmail.com>
2024-06-03 17:25:59 +1200

    renamed buffer to grid

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ typealias Grid = List<List<Boolean>>
 val rows = 40
 val cols = 60
 val directions = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
-fun neighbor(y: Int, x: Int, buffer: Grid): Int = directions.count { (dy, dx) -> buffer.get((y + dy + rows) % rows).get((x + dx + cols) % cols) }
+fun neighbor(y: Int, x: Int, grid: Grid): Int = directions.count { (dy, dx) -> grid.get((y + dy + rows) % rows).get((x + dx + cols) % cols) }
 fun set(y: Int, x: Int, grid: Grid): Boolean = if (grid[y][x]) neighbor(y, x, grid) in 2..3 else neighbor(y, x, grid) == 3
 fun step(grid: Grid): Grid = List(rows) { y -> List(cols) { x -> set(y, x, grid) } }
 fun render(grid: Grid): String = grid.joinToString("\n") { row -> row.joinToString(" ") { if (it) "#" else "-" } }

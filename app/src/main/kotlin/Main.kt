b3923074d0df7eabfb74de93418e5af0dec0426a
PippyHub <piper.innshall@gmail.com>
2024-06-03 16:45:26 +1200

    use immutable data

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,23 +1,12 @@
 import java.io.File
-
-typealias Grid = Array<BooleanArray>
-
-val rows = 20
-val cols = 30
-
+typealias Grid = List<List<Boolean>>
+val rows = 40
+val cols = 60
 val directions = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
-
-fun neighbor(y: Int, x: Int, buffer: Grid): Int = directions.count { (dy, dx) -> buffer[(y + dy + rows) % rows][(x + dx + cols) % cols] }
-
-fun set(y: Int, x: Int, buffer: Grid) = if (buffer[y][x]) neighbor(y, x, buffer) in 2..3 else neighbor(y, x, buffer) == 3
-
-fun step(buffer: Grid): Grid = Array(rows) { y -> BooleanArray(cols) { x -> set(y, x, buffer) } }
-
-fun render(grid: Grid) = "\u001b[H\u001b[2J" + grid.joinToString("\n") { it.joinToString(" ") { if (it) "#" else "-" } }
-
-fun game(grid: Grid): Unit = println(render(grid)).also { Thread.sleep(1000) }.let { game(step(grid)) }
-
-fun grid(file: File): Grid = Array(rows) { BooleanArray(cols) { false } }
-  .also { grid -> file.readLines().map { it.split(",").map(String::toInt) }.forEach { (y, x) -> grid[y][x] = true } }
-
+fun neighbor(y: Int, x: Int, buffer: Grid): Int = directions.count { (dy, dx) -> buffer.get((y + dy + rows) % rows).get((x + dx + cols) % cols) }
+fun set(y: Int, x: Int, grid: Grid): Boolean = if (grid[y][x]) neighbor(y, x, grid) in 2..3 else neighbor(y, x, grid) == 3
+fun step(grid: Grid): Grid = List(rows) { y -> List(cols) { x -> set(y, x, grid) } }
+fun render(grid: Grid): String = grid.joinToString("\n") { row -> row.joinToString(" ") { if (it) "#" else "-" } }
+fun game(grid: Grid): Unit = println("\u001b[H\u001b[2J" + render(grid)).also { Thread.sleep(100) }.let { game(step(grid)) }
+fun grid(file: File): Grid = List(rows) { y -> List(cols) { x -> file.readLines().map { it.split(",").map(String::toInt) }.any { it[0] == y && it[1] == x } } }
 fun main() = game(grid(File("src/main/resources/grid")))

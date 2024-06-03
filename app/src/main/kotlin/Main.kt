3390491b728f3a16fc6b8b552514452672d0ac11
PippyHub <piper.innshall@gmail.com>
2024-06-03 16:07:54 +1200

    arrays are mutable but lists are not...

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 typealias Grid = Array<BooleanArray>
 
 val rows = 20
@@ -11,37 +13,11 @@ fun set(y: Int, x: Int, buffer: Grid) = if (buffer[y][x]) neighbor(y, x, buffer)
 
 fun step(buffer: Grid): Grid = Array(rows) { y -> BooleanArray(cols) { x -> set(y, x, buffer) } }
 
-fun string(grid: Grid) = "\u001b[H\u001b[2J" + grid.joinToString("\n") { it.joinToString(" ") { if (it) "#" else "-" } }
+fun render(grid: Grid) = "\u001b[H\u001b[2J" + grid.joinToString("\n") { it.joinToString(" ") { if (it) "#" else "-" } }
 
-fun draw(grid: Grid) = println(string(grid)).also { Thread.sleep(500) }
+fun game(grid: Grid): Unit = println(render(grid)).also { Thread.sleep(1000) }.let { game(step(grid)) }
 
-fun game(grid: Grid): Unit = draw(grid).let { game(step(grid)) }
+fun grid(file: File): Grid = Array(rows) { BooleanArray(cols) { false } }
+  .also { grid -> file.readLines().map { it.split(",").map(String::toInt) }.forEach { (y, x) -> grid[y][x] = true } }
 
-fun createGrid(): Grid {
-    return Array(rows) { y ->
-        BooleanArray(cols) { x ->
-            when {
-                y == 0 && x == 2 -> true
-                y == 1 && x == 0 -> true
-                y == 1 && x == 2 -> true
-                y == 2 && x == 1 -> true
-                y == 2 && x == 2 -> true
-                else -> false
-            }
-        }
-    }
-}
-
-fun main() = game(createGrid())
-
-//fun render(grid: Grid) = "\u001b[H\u001b[2J" + grid.joinToString("\n") { it.joinToString(" ") { if (it) "#" else "-" } }
-
-
-//fun game(grid: Grid): Grid = println(render(grid)).also { Thread.sleep(500) }.let { game(step(grid)) }
-
-//import java.io.File
-
-//val file = File("source/main/resources/grid")
-
-//fun createGrid(): Grid = Array(rows) { BooleanArray(cols) { false } }
-//  .also { grid -> file.readLines().map { it.split(",").map(String::toInt) }.forEach { (y, x) -> grid[y][x] = true } }
+fun main() = game(grid(File("src/main/resources/grid")))
